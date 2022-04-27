3ba81402ba61a0d9458a264772d41258bb086bf8
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-04-27 11:27:07 +0900

    fix: getClotheDetail@IndexController
    modified detail button bug

--- ClotheApp/src/main/java/com/example/demo/controller/IndexController.java
+++ ClotheApp/src/main/java/com/example/demo/controller/IndexController.java
@@ -7,10 +7,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.form.ClotheDetailForm;
+import com.example.demo.form.ClotheSearchForm;
 import com.example.demo.model.Clothe;
 import com.example.demo.service.ClotheService;
 
@@ -29,12 +31,16 @@ public class IndexController {
 //	}
 	
 	@GetMapping("/index")
-	public String getClotheList(Model model) {
+	public String getClotheList(@ModelAttribute ClotheSearchForm form,Model model) {
 		
-		List<Clothe> clotheList = clotheService.getClothes();
+//		リストの取得
+		Clothe c = modelMapper.map(form, Clothe.class);
+		
+		List<Clothe> clotheList = clotheService.getClothes(c);
 		
 		model.addAttribute("clotheList", clotheList);
 		
+//		詳細画面の取得
 		Clothe clothe = clotheService.getClotheOne(clotheService.getMinId());
 		
 		ClotheDetailForm clotheDetailForm = modelMapper.map(clothe, ClotheDetailForm.class);
@@ -45,9 +51,9 @@ public class IndexController {
 	}
 		
 
-	
+//リストの詳細ボタンを押して詳細画面を表示する	
 	@PostMapping("/index")
-	public String getClotheDetail(@RequestParam("id") String str, Model model) {
+	public String getClotheDetail(@ModelAttribute ClotheSearchForm form, @RequestParam("id") String str, @RequestParam("searchWord") String searchWord, Model model) {
 		
 		Clothe clothe = clotheService.getClotheOne(str);
 		
@@ -55,9 +61,28 @@ public class IndexController {
 		
 		model.addAttribute("clotheDetailForm", clotheDetailForm);
 		
-		List<Clothe> clotheList = clotheService.getClothes();
+//		リストを取得　ここがおかしい
+//		Clothe c = new Clothe();
+//
+//		List<Clothe> clotheList = clotheService.getClothes(c);
+//		
+//		model.addAttribute("clotheList", clotheList);
+//		
+//		ClotheSearchForm csf = new ClotheSearchForm();
+//		
+//		model.addAttribute("clotheSearchForm", csf);
 		
+//		検索をかけた後に二回連続detailボタン押したとき検索結果がリセットされてしまう
+		
+		form.setClotheName(searchWord);
+		
+		Clothe c = modelMapper.map(form, Clothe.class);
+
+		List<Clothe> clotheList = clotheService.getClothes(c);
+
 		model.addAttribute("clotheList", clotheList);
+//		search form の検索ワード消去
+//		form.setClotheName(null);
 		
 		return "layout/layout";
 	}
@@ -76,6 +101,29 @@ public class IndexController {
 		clotheService.deleteClotheOne(form.getClotheId());
 		
 		return "redirect:/index";
+	}
+	
+//	Clothes検索処理
+	@PostMapping(value= "/index", params = "search")
+	public String searchClotheList(@ModelAttribute ClotheSearchForm form, Model model) {
+		
+		Clothe clothe = modelMapper.map(form, Clothe.class);
+		
+		List<Clothe> clotheList = clotheService.getClothes(clothe);
+		
+		model.addAttribute("clotheList", clotheList);
+		
+//		詳細画面のための取得
+//		検索したあとの最小ID値の詳細画面取得実装していない
+		
+		Clothe clotheForDetail = clotheList.get(0);
+		/* Clothe c = clotheService.getClotheOne(clotheService.getMinId()); */
+
+		ClotheDetailForm clotheDetailForm = modelMapper.map(clotheForDetail, ClotheDetailForm.class);
+
+		model.addAttribute("clotheDetailForm", clotheDetailForm);
+		
+		return "layout/layout";
 	}
 	
 //	@PostMapping("/index")
