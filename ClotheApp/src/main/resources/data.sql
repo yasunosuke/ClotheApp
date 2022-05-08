145ecd880795dd361ffbd2b474f41bb179f4ec16
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-05-09 02:13:57 +0900

    feat: insert some new rows into categories

--- ClotheApp/src/main/resources/data.sql
+++ ClotheApp/src/main/resources/data.sql
@@ -1,4 +1,6 @@
-INSERT INTO categories(category_id, category_name) VALUES('1', 'pants'),('2','t-shirts');
+INSERT INTO categories(category_id, category_name) VALUES('1', 'pants'),('2','tshirts'),
+('3','shirts'),('4','longshirts'),('5','hoodie'),('6','shorts'),('7','scurf'),('8','shoes'),
+('9','tanktop'),('10','glasses'),('11','sports');
 
 INSERT INTO clothes(
 clothe_id,
