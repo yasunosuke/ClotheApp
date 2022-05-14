d655e648b9596a11f69489d32ae65dc1e6d03a5a
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-05-14 16:20:38 +0900

    feat: new tables
    Added new table named storages that manage where you put your clothes

--- ClotheApp/src/main/resources/schema.sql
+++ ClotheApp/src/main/resources/schema.sql
@@ -1,17 +1,23 @@
 DROP TABLE IF EXISTS clothes;
 DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS storages;
 
 CREATE TABLE IF NOT EXISTS categories(
 category_id CHAR(2) PRIMARY KEY,
-category_name CHAR(30) NOT NULL
+category_name CHAR(30) NOT NULL UNIQUE
 );
 
+CREATE TABLE IF NOT EXISTS storages(
+storage_code CHAR(2) PRIMARY KEY,
+storage_name VARCHAR(50) NOT NULL UNIQUE);
+
 CREATE TABLE IF NOT EXISTS clothes(
-clothe_image MEDIUMBLOB
-,clothe_id CHAR(50) PRIMARY KEY
-,clothe_name CHAR(50) NOT NULL
+clothe_id CHAR(50) PRIMARY KEY
+,clothe_image MEDIUMBLOB
+,clothe_name VARCHAR(50) NOT NULL
 ,category_id CHAR(2) NOT NULL
-,storage CHAR(30) NOT NULL
+,storage_code CHAR(2) NOT NULL
 ,registered_date DATE NOT NULL
 ,FOREIGN KEY (category_id) REFERENCES categories(category_id)
+,FOREIGN KEY (storage_code) REFERENCES storages(storage_code)
 );
