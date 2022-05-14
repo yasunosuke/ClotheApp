0fd18dda70a2dd3e53d8def32059f7a038c9709e
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-05-14 16:21:25 +0900

    feat: insert new data
    Inserted new initial data to storages

--- ClotheApp/src/main/resources/data.sql
+++ ClotheApp/src/main/resources/data.sql
@@ -2,13 +2,16 @@ INSERT INTO categories(category_id, category_name) VALUES('1', 'pants'),('2','ts
 ('3','shirts'),('4','longshirts'),('5','hoodie'),('6','shorts'),('7','scurf'),('8','shoes'),
 ('9','tanktop'),('10','glasses'),('11','sports');
 
+INSERT INTO storages(storage_code, storage_name) VALUES('1', 'FIRST DRAWER FROM TOP AT MY ROOM CLOSET'),
+('2','HANGER POLE AT MY ROOM'),('3', 'FISRT DRAWER FROM TOP AT MOM\'s ROOM CLOSET');
+
 INSERT INTO clothes(
 clothe_id,
 clothe_name,
 category_id,
-storage,
+storage_code,
 registered_date
 )VALUES
-('1', 'Nike Pants', '1', 'closet', CURRENT_DATE()),
-('2','Adidas T-shirts', '2', 'drawer', '2000-01-01');
+('1', 'Nike Pants', '1', '1', CURRENT_DATE()),
+('2','Adidas T-shirts', '2', '2', '2000-01-01');
 
