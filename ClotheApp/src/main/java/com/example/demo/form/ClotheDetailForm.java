26e0fd27040f8d41e1e621a52992d8bf726104f1
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-05-14 16:24:25 +0900

    feat: Add some fields
    To set data from storages and categories

--- ClotheApp/src/main/java/com/example/demo/form/ClotheDetailForm.java
+++ ClotheApp/src/main/java/com/example/demo/form/ClotheDetailForm.java
@@ -3,15 +3,18 @@ package com.example.demo.form;
 import java.util.Date;
 
 import com.example.demo.model.Category;
+import com.example.demo.model.Storage;
 
 import lombok.Data;
 
 @Data
 public class ClotheDetailForm {
-	private byte[] clotheImage;
+//	private byte[] clotheImage;
 	private String clotheId;
 	private String clotheName;
 	private String categoryId;
-	private String storage;
+	private String storageCode;
 	private Date registeredDate;
+	private Category category;
+	private Storage storage;
 }
