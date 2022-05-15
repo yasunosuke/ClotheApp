ffb3820d45ac904e45ee5a2bce0917525fe63d41
yasunosuke <42140495+yasunosuke@users.noreply.github.com>
2022-05-15 21:28:39 +0900

    feat: New class

---
+++ ClotheApp/src/test/java/com/example/demo/controller/IndexControllerTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndexControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	
+
+}
